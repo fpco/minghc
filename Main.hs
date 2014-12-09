2f15ed52cacdc225f496d91ef3753f409ec9948c
Neil Mitchell <ndmitchell@gmail.com>
2014-12-09 13:58:30 +0000

    Replace downloads.sh with a Shake based Haskell script

--- Main.hs
+++ Main.hs
@@ -1,42 +1,35 @@
-{-# LANGUAGE OverloadedStrings #-}
 
 module Main(main) where
 
-import Development.NSIS
-import Development.NSIS.Plugins.EnvVarUpdate
+import Development.Shake
+import Installer
 
 main :: IO ()
-main = writeFile "ltshaskell.nsi" $ nsis $ do
-    name "LTSHaskell"
-    outFile "LTSHaskell.exe"
-    installDir "$PROGRAMFILES/LTSHaskell"
-    requestExecutionLevel User
+main = shake shakeOptions $ do
+    want ["ltshaskell.exe"]
 
-    page Directory
-    page InstFiles
+    ".cabal-1.20" %> \out -> do
+        () <- cmd "wget --no-check-certificate https://www.haskell.org/cabal/release/cabal-install-1.20.0.3/cabal-1.20.0.3-i386-unknown-mingw32.tar.gz"
+        () <- cmd "rm -rf cabal-1.20"
+        () <- cmd "tar zxfv cabal-1.20.0.3-i386-unknown-mingw32.tar.gz"
+        () <- cmd "mkdir -p cabal-1.20/bin"
+        () <- cmd "mv cabal.exe cabal-1.20/bin"
+        writeFile' out ""
 
-    section "" [] $ do
-        setOutPath "$INSTDIR"        -- Where to install files in this section
-        writeUninstaller "uninstall.exe"
+    ".ghc-7.8.3" %> \out -> do
+        () <- cmd "wget --no-check-certificate https://www.haskell.org/ghc/dist/7.8.3/ghc-7.8.3-i386-unknown-mingw32.tar.xz"
+        () <- cmd "rm -rf ghc-7.8.3"
+        () <- cmd "tar xf ghc-7.8.3-i386-unknown-mingw32.tar.xz"
+        writeFile' out ""
 
-        file [Recursive] "ghc-7.8.3"
-        file [Recursive] "cabal-1.20"
-        file [Recursive] "msys-1.0"
+    ".msys-1.0" %> \out -> do
+        () <- cmd "rm -rf msys-1.0"
+        () <- cmd "unzip msys-1.0.zip"
+        writeFile' out ""
 
-        -- Should use HKLM instead of HKCU for all but APPDATA.
-        -- However, we need to ensure that the APPDATA path comes first.
-        -- And this is the only way I could make that happen.
+    "ltshaskell.exe" %> \out -> do
+        need ["ltshaskell.nsi",".cabal-1.20",".ghc-7.8.3",".msys-1.0"]
+        cmd "makensis ltshaskell.nsi"
 
-        setEnvVarAppend HKCU "PATH" "$APPDATA/cabal/bin"
-        setEnvVarAppend HKCU "PATH" "$INSTDIR/ghc-7.8.3/bin"
-        setEnvVarAppend HKCU "PATH" "$INSTDIR/ghc-7.8.3/mingw/bin"
-        setEnvVarAppend HKCU "PATH" "$INSTDIR/cabal-1.20/bin"
-        setEnvVarAppend HKCU "PATH" "$INSTDIR/msys-1.0/bin"
-
-    uninstall $ do
-        rmdir [Recursive] "$INSTDIR"
-        setEnvVarRemove HKCU "PATH" "$INSTDIR/msys-1.0/bin"
-        setEnvVarRemove HKCU "PATH" "$INSTDIR/cabal-1.20/bin"
-        setEnvVarRemove HKCU "PATH" "$INSTDIR/ghc-7.8.3/mingw/bin"
-        setEnvVarRemove HKCU "PATH" "$INSTDIR/ghc-7.8.3/bin"
-        setEnvVarRemove HKCU "PATH" "$APPDATA/cabal/bin"
+    "ltshaskell.nsi" %> \out -> do
+        writeFile' out installer
