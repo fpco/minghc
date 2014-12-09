0803b5e5a302dc4c4e8e0b64dec7575ac40044f7
Neil Mitchell <ndmitchell@gmail.com>
2014-12-09 14:31:52 +0000

    Use less shell script, download to a separate file

--- Main.hs
+++ Main.hs
@@ -2,30 +2,43 @@
 module Main(main) where
 
 import Development.Shake
+import Development.Shake.FilePath
+import Control.Exception.Extra
+import Data.List.Extra
+import System.Directory
 import Installer
 
 main :: IO ()
 main = shake shakeOptions $ do
     want ["ltshaskell.exe"]
 
+    "cabal-*.tar.gz" %> \out -> do
+        let ver = splitOn "-" (dropExtension $ takeBaseName out) !! 1
+        let url = "https://www.haskell.org/cabal/release/cabal-install-" ++ ver ++ "/cabal-" ++ ver ++ "-i386-unknown-mingw32.tar.gz"
+        cmd "wget --no-check-certificate" url "-O" out
+
+    "ghc-*.tar.bz2" %> \out -> do
+        let ver = splitOn "-" (dropExtension $ takeBaseName out) !! 1
+        let url = "https://www.haskell.org/ghc/dist/" ++ ver ++ "/ghc-" ++ ver ++ "-i386-unknown-mingw32.tar.bz2"
+        cmd "wget --no-check-certificate" url "-O" out
+
     ".cabal-1.20" %> \out -> do
-        () <- cmd "wget --no-check-certificate https://www.haskell.org/cabal/release/cabal-install-1.20.0.3/cabal-1.20.0.3-i386-unknown-mingw32.tar.gz"
-        () <- cmd "rm -rf cabal-1.20"
-        () <- cmd "tar zxfv cabal-1.20.0.3-i386-unknown-mingw32.tar.gz"
-        () <- cmd "mkdir -p cabal-1.20/bin"
-        () <- cmd "mv cabal.exe cabal-1.20/bin"
         writeFile' out ""
+        need ["cabal-1.20.0.3.tar.gz"]
+        liftIO $ ignore $ removeDirectoryRecursive "cabal-1.20"
+        liftIO $ createDirectoryIfMissing True "cabal-1.20/bin"
+        cmd "tar zxfv cabal-1.20.0.3.tar.gz -C cabal-1.20/bin"
 
     ".ghc-7.8.3" %> \out -> do
-        () <- cmd "wget --no-check-certificate https://www.haskell.org/ghc/dist/7.8.3/ghc-7.8.3-i386-unknown-mingw32.tar.bz2"
-        () <- cmd "rm -rf ghc-7.8.3"
-        () <- cmd "tar xf ghc-7.8.3-i386-unknown-mingw32.tar.bz2"
         writeFile' out ""
+        need ["ghc-7.8.3.tar.bz2"]
+        liftIO $ ignore $ removeDirectoryRecursive "ghc-7.8.3"
+        cmd "tar xf ghc-7.8.3.tar.bz2"
 
     ".msys-1.0" %> \out -> do
-        () <- cmd "rm -rf msys-1.0"
-        () <- cmd "unzip msys-1.0.zip"
         writeFile' out ""
+        liftIO $ ignore $ removeDirectoryRecursive "msys-1.0"
+        cmd "unzip msys-1.0.zip"
 
     "ltshaskell.exe" %> \out -> do
         need ["ltshaskell.nsi",".cabal-1.20",".ghc-7.8.3",".msys-1.0"]
