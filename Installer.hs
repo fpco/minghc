5e6a3e29ca259b1fa33637db3f10de4daf3a016d
Michael Snoyman <michael@snoyman.com>
2015-05-07 11:20:30 +0300

    Remove highest execution level

--- Installer.hs
+++ Installer.hs
@@ -20,7 +20,6 @@ installer arch version = nsis $ do
     outFile "minghc-$GHC-$ARCH.exe"
     -- See: http://stackoverflow.com/questions/1831810/is-appdata-now-the-correct-place-to-install-user-specific-apps-which-modify-t/1845459#1845459
     installDir "$LOCALAPPDATA/Programs/minghc-$GHC-$ARCH"
-    requestExecutionLevel Highest
 
     page Components
     page Directory
